9ae2ad6ec1f23e98eb3f9e3c45245d39917cd126
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:44:45 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "glue_security_configurations_id" {
 }
 output "glue_security_configurations_encryption_configuration" {
   description = "Map of encryption_configuration values across all glue_security_configurations, keyed the same as var.glue_security_configurations"
-  value       = { for k, v in aws_glue_security_configuration.glue_security_configurations : k => v.encryption_configuration if v.encryption_configuration != null && length(v.encryption_configuration) > 0 }
+  value       = { for k, v in aws_glue_security_configuration.glue_security_configurations : k => one(v.encryption_configuration) if v.encryption_configuration != null && length(v.encryption_configuration) > 0 }
 }
 output "glue_security_configurations_name" {
   description = "Map of name values across all glue_security_configurations, keyed the same as var.glue_security_configurations"
